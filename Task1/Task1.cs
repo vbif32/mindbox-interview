2a6f9ccfe2ad6867c8d9e1ff71458d87f9df25a6
Vbif32 <Vbif.32@mail.ru>
2017-02-05 12:57:05 +0300

    Task1 update

--- Task1/Task1.cs
+++ Task1/Task1.cs
@@ -9,11 +9,26 @@ namespace Task1
         public static void Main(string[] args)
         {
             var program = new Task1();
-            program.Run();
+
+            string nextAction = null;
+            while (nextAction != "q")
+            {
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.Write("Do you want to enter auto test (a), manual test (m) or quit (q)?  ");
+                nextAction = Console.ReadLine();
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.Write("\n");
+
+                if (nextAction == "a")
+                    program.AutomaticControl();
+                if (nextAction == "m")
+                    program.ManualControl();
+
+                Console.Write("\n\n");
+            }
         }
-
-
-        private void Run()
+        
+        private void ManualControl()
         {
             var cards = new List<Card>();
 
@@ -21,8 +36,9 @@ namespace Task1
             {
                 if (cards.Count > 0)
                 {
-                    Console.WriteLine($"You have entered {cards.Count} cards. Do you want to enter another card? (y/n)");
+                    Console.Write($"You have entered {cards.Count} cards. Do you want to enter another card (y/n) ?  ");
                     var next = Console.ReadLine();
+                    Console.Write("\n");
 
                     if (next == "n")
                         break;
@@ -30,48 +46,104 @@ namespace Task1
                         continue;
                 }
 
-                Console.Write("Input departure city: ");
+                Console.Write("Input  departure  city: ");
                 var from = Console.ReadLine();
-
                 Console.Write("Input destination city: ");
                 var to = Console.ReadLine();
+                Console.Write("\n");
 
                 cards.Add(new Card(from, to));
             }
+            var sortedCards = SortCards(cards);
+            AutoTest(sortedCards);
+            PrintCardsForManualTest(cards, sortedCards);
+        }
 
-            Console.WriteLine("Unsorted cards:");
-            foreach (var card in cards)
-                Console.WriteLine(card.ToString());
+        private void AutomaticControl()
+        {
+            var cards = CreateCards();
+            var mixedCards = MixCards(cards);
+            var sortedCards = SortCards(mixedCards);
 
-            cards = SortCards(cards);
-            Console.WriteLine("Sorted cards:");
-            Console.WriteLine(cards);
+            AutoTest(sortedCards);
+            PrintCardsForManualTest(mixedCards, sortedCards);
         }
 
         public List<Card> SortCards(List<Card> cards)
         {
             var mixedCards = new List<Card>(cards);
-            var firstCard = GetFirstCard(mixedCards);
-            var sortedCards = new List<Card> {firstCard};
+            var nextCity = FindStartCity(mixedCards);
+            var sortedCards = new List<Card>(cards.Count);
 
-            while (mixedCards.Count > 1)
+            while (mixedCards.Count > 0)
             {
-                var lastAddedCard = sortedCards.Last();
-                mixedCards.Remove(lastAddedCard);
-                var newCard = mixedCards.First(c => c.From == lastAddedCard.To);
-                sortedCards.Add(newCard);
+                var nextCard = mixedCards.First(c => c.From == nextCity);
+                sortedCards.Add(nextCard);
+                mixedCards.Remove(nextCard);
+                nextCity = nextCard.To;
             }
             return sortedCards;
         }
 
-        public Card GetFirstCard(List<Card> cards)
+        public string FindStartCity(List<Card> cards)
         {
             var departureCities = cards.Select(c => c.From);
             var destinationCities = cards.Select(c => c.To);
 
             var firstCity = departureCities.Except(destinationCities).First();
 
-            return cards.First(c => c.From == firstCity);
+            return firstCity;
+        }
+
+        private void PrintCardsForManualTest(IEnumerable<Card> mixedCards, IEnumerable<Card> sortedCards)
+        {
+            Console.WriteLine("Unsorted cards:");
+            foreach (var card in mixedCards)
+                Console.WriteLine(card.ToString());
+
+            Console.Write("\n");
+
+            Console.WriteLine("Sorted cards:");
+            foreach (var card in sortedCards)
+                Console.WriteLine(card.ToString());
+        }
+
+        private void AutoTest(List<Card> sortedCards)
+        {
+            for (var i = 0; i < sortedCards.Count-1; i++)
+                if (sortedCards[i].To != sortedCards[i + 1].From)
+                    Console.WriteLine($"{i} card isn't correct");
+        }
+
+        public static List<Card> CreateCards()
+        {
+            return new List<Card>
+            {
+                new Card(City.BuenosAires.ToString(), City.Canberra.ToString()),
+                new Card(City.Canberra.ToString(), City.Cologne.ToString()),
+                new Card(City.Cologne.ToString(), City.London.ToString()),
+                new Card(City.London.ToString(), City.Melbourne.ToString()),
+                new Card(City.Melbourne.ToString(), City.Minsk.ToString()),
+                new Card(City.Minsk.ToString(), City.Moscow.ToString()),
+                new Card(City.Moscow.ToString(), City.Paris.ToString()),
+                new Card(City.Paris.ToString(), City.Vatican.ToString()),
+                new Card(City.Vatican.ToString(), City.Vein.ToString())
+            };
+        }
+
+        public static List<Card> MixCards(List<Card> cards)
+        {
+            var mixedCards = new List<Card>(cards);
+            var random = new Random();
+
+            for (var i = 0; i < mixedCards.Count; i++)
+            {
+                var randomPosition = random.Next(0, mixedCards.Count);
+                var tmpCard = mixedCards[randomPosition];
+                mixedCards[randomPosition] = mixedCards[i];
+                mixedCards[i] = tmpCard;
+            }
+            return mixedCards;
         }
     }
 }
